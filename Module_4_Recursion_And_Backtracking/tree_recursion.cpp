f1db54ebc1581d2e445d944cbcd70735c6d9ee43
S. M. Mazharul Islam Rifat <65125911+S-M-Mazharul-Islam-RIfat@users.noreply.github.com>
2026-02-05 19:17:17 +0600

    added module 4 codes

---
+++ Module_4_Recursion_And_Backtracking/tree_recursion.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n) {
+   if (n == 0) {
+      return;
+   }
+   cout << n << " ";
+   print(n - 1);
+   print(n - 1);
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int n;
+   cin >> n;
+   print(n);
+   return 0;
+}
